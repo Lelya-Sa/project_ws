60923ba3249df596b72cbcd8c32932534ef95166
lelyasa <sa.lelya@campus.technion.ac.il>
2024-10-15 16:26:06 +0300

    init

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/lelyasa/project_ws/src
+# Build directory: /home/lelyasa/project_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("arm_moveit_config")
+subdirs("my_simulations")
+subdirs("arm_description")
+subdirs("pysdf")
---
+++ build/mixed_robot_model_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lelyasa/project_ws/src/mixed_robot_model_pkg
+# Build directory: /home/lelyasa/project_ws/build/mixed_robot_model_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
