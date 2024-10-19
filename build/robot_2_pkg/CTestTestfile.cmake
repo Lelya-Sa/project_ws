afe719a463d0ba885a02172ea37ee4e0077d37c5
lelyasa <sa.lelya@campus.technion.ac.il>
2024-10-19 20:36:55 +0300

    init

---
+++ build/robot_2_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lelyasa/project_ws/src/robot_2_pkg
+# Build directory: /home/lelyasa/project_ws/build/robot_2_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
