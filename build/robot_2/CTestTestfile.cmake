f92bf6283ce4a94737aa41e509332d64f38f9fd2
lelyasa <sa.lelya@campus.technion.ac.il>
2024-10-24 07:30:21 +0300

    robot_2 works

---
+++ build/robot_2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lelyasa/project_ws/src/robot_2
+# Build directory: /home/lelyasa/project_ws/build/robot_2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
