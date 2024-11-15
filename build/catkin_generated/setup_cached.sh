0c5e88a6b0911b8ee91dbf370f2728cc8fa81fee
lelyasa <sa.lelya@campus.technion.ac.il>
2024-11-15 12:13:35 +0200

    camkelist

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,9 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/lelyasa/project_ws/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH='/opt/ros/noetic/lib:/opt/ros/noetic/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu/gazebo-11/plugins:/usr/lib/x86_64-linux-gnu/gazebo-11/plugins:/usr/lib/x86_64-linux-gnu/gazebo-11/plugins'
-export PATH='/opt/ros/noetic/bin:/usr/bin:/bin'
 export PWD='/home/lelyasa/project_ws/build'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/lelyasa/project_ws/devel/share/common-lisp'
-export ROS_PACKAGE_PATH="/home/lelyasa/project_ws/src:$ROS_PACKAGE_PATH"
